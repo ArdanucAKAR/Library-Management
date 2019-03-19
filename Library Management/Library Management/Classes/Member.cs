0260336c980957e5e0190ff17354d9ed74945fbc
volkankahraman <khrmnvolkan@gmail.com>
2019-03-19 13:00:23 +0300

    class ve interface eklendi

---
+++ Library Management/Library Management/Classes/Member.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library_Management
+{
+    public class Member : Ioperations
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Library Management/Library Management/Interfaces/Ioperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management
+{
+    interface Ioperations
+    {
+        void Add();
+
+        void Update();
+
+        void Delete();
+    }
+}
