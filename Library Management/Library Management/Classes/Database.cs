88ea5c6711f2fcb8f042f33d152f8b820ec6d0d5
volkankahraman <khrmnvolkan@gmail.com>
2019-03-20 23:05:36 +0300

    login eklendi

---
+++ Library Management/Library Management/Classes/Database.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Library_Management.Classes
+{
+    public class Database
+    {
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["sqlConn"].ConnectionString);
+
+        public string Connect(string username, string password)
+        {
+            try
+            {
+                connection.Open();
+                if ((connection.State & ConnectionState.Open) > 0)
+                {
+                    return "Connection OK!";
+                }
+                else
+                {
+                    return "No Connection!";
+
+                }
+            }
+            catch (Exception err)
+            {
+                return "No Connection!";
+            }
+        }
+        public bool login(string username, string password)
+        {
+            return true;
+        }
+    }
+}
